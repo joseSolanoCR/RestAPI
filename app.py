9e28f2198a7431473381ea39054f54f55e3d0234
jpsol <jsolanoca@ice.go.cr>
2022-12-15 08:14:12 -0600

    save

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-from flask import Flask
-
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
+stores = [
+        {
+                "name": "my store",
+                "items": [
+                        {
+                                "name": "chair",
+                                "price": 15.99
+                        }
+                ]
+
+        }
+
+]
+
+
+@app.get("/store")
+def get_stores():
+        return{"stores":stores}
+
+@app.post("/store")
+def create_store():
+        request_data = request.get_json()
+        new_store = {"name": request_data["name"], "items":[]}
+        stores.append(new_store)
+        return new_store, 201
 
 if __name__ == "__main__":
         app.run(host = 'localhost', debug = True)
