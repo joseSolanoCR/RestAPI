e5d0a71d12722dcbb7f8c4cc4c1c62a5dc400039
jpsol <jsolanoca@ice.go.cr>
2022-12-15 10:25:16 -0600

    save

--- app.py
+++ app.py
@@ -2,32 +2,47 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
 stores = [
-        {
-                "name": "my store",
-                "items": [
-                        {
-                                "name": "chair",
-                                "price": 15.99
-                        }
-                ]
+    {
+        "name": "my store",
+        "items": [
+            {
+                "name": "chair",
+                "price": 15.99
+            }
+        ]
 
-        }
+    }
 
 ]
 
 
 @app.get("/store")
 def get_stores():
-        return{"stores":stores}
+    return {"stores": stores}
+
 
 @app.post("/store")
 def create_store():
-        request_data = request.get_json()
-        new_store = {"name": request_data["name"], "items":[]}
-        stores.append(new_store)
-        return new_store, 201
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store,  201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    print(request_data)
+    print(name)
+    for store in stores:
+        print(store)
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
 
 if __name__ == "__main__":
-        app.run(host = 'localhost', debug = True)
+    app.run(host='localhost', debug=True)
