9c8d859db3264320e5572d2441cc103a740f42e3
jpsol <jsolanoca@ice.go.cr>
2022-12-20 16:11:16 -0600

    modelos de datos con SQLAlchemy

--- resources/item.py
+++ resources/item.py
@@ -1,54 +1,60 @@
-import uuid
-from flask import request
 from flask.views import MethodView
-from flask_smorest import Blueprint,abort
-from db import items
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
-
-blp = Blueprint("items", __name__, description="Operations in items")
+blp = Blueprint("Items", "items", description="Operations on items")
 
 
 @blp.route("/item/<string:item_id>")
-class Store(MethodView):
+class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Store not found")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "store deleted"}
-        except KeyError:
-            abort(404, message="item not found")
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."}
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
-            item |= item_data
-            return item
-        except KeyError:
-            abort(400, message="Item not found")
+        item = ItemModel.query.get(item_id)
+
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id,**item_data)
+
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
 
 
 @blp.route("/item")
 class ItemList(MethodView):
     @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": items.values()}
+        return ItemModel.query.all()
 
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
-    def post(self,item_data):
-        for item in items.values():
-            if item_data["name"] == item["name"]:
-                abort(404, message="Store already exists")
-        store_id = uuid.uuid4().hex
-        store = {**item_data, "id": store_id}
-        items[store_id] = store
-        return store, 201
+    def post(self, item_data):
+        item = ItemModel(**item_data)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
+
+        return item
--- schemas.py
+++ schemas.py
@@ -2,26 +2,27 @@ from marshmallow import Schema, fields
 
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
 
 class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
-
-
-class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True, load_only=True)
-    store = fields.Nested(lambda: PlainStoreSchema(), dump_only=True)
+    store_id = fields.Int()
 
 
 class StoreSchema(PlainStoreSchema):
-    items = fields.List(fields.Nested(lambda: PlainItemSchema()), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
 
