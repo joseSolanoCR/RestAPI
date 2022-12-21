6b1cd3584a53be2f5a6534ebfa923ae1c68686fc
jpsol <jsolanoca@ice.go.cr>
2022-12-21 12:34:36 -0600

    etiquetas (relaciones tienda-etiqueta)

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from db import db
 import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 
 def create_app(db_url=None):
@@ -26,6 +27,7 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
 
